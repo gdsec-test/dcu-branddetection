9f41160f367d73a93624fa4ec602df4d326bf305
ebenson <ebenson@godaddy.com>
2017-09-18 14:34:41 -0700

    update godaddybrand_tests.py

--- tests/godaddybrand_tests.py
+++ tests/godaddybrand_tests.py
@@ -5,67 +5,46 @@ from branddetection.brands.godaddybrand import GoDaddyBrand
 
 
 class TestGoaddyBrand:
-    def __init__(self):
-        pass
 
     @patch.object(ASNPrefixes, '_ripe_get_prefixes_per_asn')
-    def test_hostname_is_hosted(self, _ripe_get_prefixes_per_asn):
+    def __init__(self, _ripe_get_prefixes_per_asn):
         _ripe_get_prefixes_per_asn.return_value = []
-        gdb = GoDaddyBrand()
+        self._gdb = GoDaddyBrand()
 
+    def test_hostname_is_hosted(self):
         test_value = {'hosting_company_name': 'GoDaddy.com, LLC'}
 
-        result = gdb.is_hosted(test_value)
+        result = self._gdb.is_hosted(test_value)
         assert_true(result is True)
 
-    @patch.object(ASNPrefixes, '_ripe_get_prefixes_per_asn')
-    def test_rdns_is_hosted(self, _ripe_get_prefixes_per_asn):
-        _ripe_get_prefixes_per_asn.return_value = []
-        gdb = GoDaddyBrand()
-
+    def test_rdns_is_hosted(self):
         test_value = {'hosting_company_name': 'UK-WEBFUSION-LEEDS', 'ip': '208.109.192.70'}
 
-        result = gdb.is_hosted(test_value)
+        result = self._gdb.is_hosted(test_value)
         assert_true(result is True)
 
-    @patch.object(ASNPrefixes, '_ripe_get_prefixes_per_asn')
-    def test_false_is_hosted(self, _ripe_get_prefixes_per_asn):
-        _ripe_get_prefixes_per_asn.return_value = []
-        gdb = GoDaddyBrand()
-
+    def test_false_is_hosted(self):
         test_value = {'hosting_company_name': 'UK-WEBFUSION-LEEDS', 'ip': '212.48.64.1'}
 
-        result = gdb.is_hosted(test_value)
+        result = self._gdb.is_hosted(test_value)
         assert_true(result is False)
 
-    @patch.object(ASNPrefixes, '_ripe_get_prefixes_per_asn')
-    def test_godaddy_is_registered(self, _ripe_get_prefixes_per_asn):
-        _ripe_get_prefixes_per_asn.return_value = []
-        gdb = GoDaddyBrand()
-
+    def test_godaddy_is_registered(self):
         test_value = {'registrar_name': 'GoDaddy.com, LLC'}
 
-        result = gdb.is_registered(test_value)
+        result = self._gdb.is_registered(test_value)
         assert_true(result is not None)
 
-    @patch.object(ASNPrefixes, '_ripe_get_prefixes_per_asn')
-    def test_notgodaddy_is_registered(self, _ripe_get_prefixes_per_asn):
-        _ripe_get_prefixes_per_asn.return_value = []
-        gdb = GoDaddyBrand()
-
+    def test_notgodaddy_is_registered(self):
         test_value = {'registrar_name': 'CSC CORPORATE DOMAINS, INC.'}
 
-        result = gdb.is_registered(test_value)
+        result = self._gdb.is_registered(test_value)
         assert_true(result is None)
 
-    @patch.object(ASNPrefixes, '_ripe_get_prefixes_per_asn')
     @patch.object(ASNPrefixes, 'get_network_for_ip')
-    def test_is_ip_in_range(self, get_network_for_ip, _ripe_get_prefixes_per_asn):
+    def test_is_ip_in_range(self, get_network_for_ip):
         get_network_for_ip.return_value = ['208.109.192.70']
-        _ripe_get_prefixes_per_asn.return_value = []
-        gdb = GoDaddyBrand()
-
         test_value = '208.109.192.70'
 
-        result = gdb.is_ip_in_range(test_value)
+        result = self._gdb.is_ip_in_range(test_value)
         assert_true(result == ['208.109.192.70'])
