f1b390bccdbfa6bdbd497425a87d3edc20b1d4b5
nwade-godaddy <80975008+nwade-godaddy@users.noreply.github.com>
2021-09-16 07:15:02 -0500

    Enforce JWT expiration rules for callers (#11)

--- branddetection/utils/auth_tools.py
+++ branddetection/utils/auth_tools.py
@@ -2,7 +2,8 @@ import os
 
 from dcustructuredloggingflask.flasklogger import get_logging
 from flask import Response, request
-from gd_auth.token import AuthToken
+from gd_auth.exceptions import TokenExpiredException
+from gd_auth.token import AuthToken, TokenBusinessLevel
 
 from settings import config_by_name
 
@@ -23,15 +24,22 @@ def authenticate_jwt():
 
 
 def validate_auth(jwt):
+    invalid_response = Response('Authentication invalid. Please verify your token', status=401)
     try:
         payload = AuthToken.payload(jwt)
         if not payload:
-            return Response('Authentication invalid. Please verify your token \n', status=401)
+            return invalid_response
         typ = payload.get('typ')
         parsed = AuthToken.parse(jwt, app_settings.SSO_URL, app="Brand Detection", typ=typ)
+
+        try:
+            parsed.is_expired(TokenBusinessLevel.LOW)
+        except TokenExpiredException:
+            return invalid_response
+
         if parsed.subject.get('cn', '') not in app_settings.CN_WHITELIST:
             logger.info('CN: {} is not whitelisted'.format(parsed.subject.get('cn')))
-            return Response('Forbidden. Please verify your token \n', status=403)
+            return Response('Forbidden. Please verify your token', status=403)
     except Exception as e:
-        logger.error('Authentication invalid with error: {}'.format(e.message))
-        return Response('Authentication invalid. Please verify your token \n', status=401)
+        logger.error(f'Authentication invalid with error: {e}')
+        return invalid_response
